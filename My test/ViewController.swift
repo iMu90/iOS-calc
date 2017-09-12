d619fdc07eb09cdc3e4f26b783b21d7a9bfd9a17
iMu90 <mtb.deco@gmail.com>
2017-09-12 09:20:49 +0300

    clone calculator app

---
+++ My test/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  My test
+//
+//  Created by Muteb Alolayan on 9/12/17.
+//  Copyright © 2017 Muteb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    /* label */
+    @IBOutlet var display: UILabel!
+    
+    /**
+     * check if the user clicks on any of the operations
+     */
+    var toReset: Bool = false
+    var number1: String!
+    var operation: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /**
+     * this function is to check whether the dispaly
+     * has a valid number 
+     * any number other than the default value (0)
+     * @params screen
+     */
+    func isValid(value: String) -> Bool
+    {
+        if(value == "0")
+        {
+            return false
+        }
+        return true
+    }
+    
+    
+//    /**
+//     * this function will conver the display value
+//     * from string to float
+//     * @params screen
+//     */
+//    func toFloat(value: String) -> Float
+//    {
+//        return Float(value)!
+//    }
+    
+    /**
+     * this function will reset the display 
+     * to default value (0)
+     * @params screen
+     */
+    
+    
+    
+    
+    /**
+     * this function will change the number sign
+     * either positive or negative
+     * @params screen
+     */
+    @IBAction func chnageSign(_ sender: Any) {
+        
+        // check if the value is NOT 0
+        if(isValid(value: display.text!)){
+            let num = Float(display.text!)! * -1
+            self.display.text = String(num)
+        }
+    }
+    
+    
+    /**
+     * this function will divide the dsiplay number
+     * by a 100
+     * @params screen
+     */
+    @IBAction func percentage(_ sender: Any) {
+        if(isValid(value: display.text!))
+        {
+            let num = Float(display.text!)! / 100
+            self.display.text = String(num)
+        }
+    }
+    
+    
+    @IBAction func divide(_ sender: Any) {
+        
+        if(isValid(value: display.text!)){
+            self.number1 = display.text!
+            self.toReset = true
+            self.operation = "divide"
+        }
+    }
+    
+    
+    @IBAction func num(_ sender: UIButton) {
+        let nim = sender.titleLabel?.text!
+        self.display.text = nim
+    }
+
+    
+    @IBAction func toClear(_ sender: Any) {
+        self.display.text = "0"
+    }
+    
+
+}
+
